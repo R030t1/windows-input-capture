fa5ef801885623a1437db791adec8644a430a752
Sid Spry <github@sid.aeam.us>
2021-04-15 17:03:57 -0500

    Add basic wpf event driven code

---
+++ 00_csharp_events/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using static PInvoke.Kernel32;
+
+namespace _00_csharp_events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs rea)
+        {
+            AllocConsole();
+        }
+
+        private void MainWindow_MouseMove(object sender, MouseEventArgs mea)
+        {
+            Console.WriteLine("mouse");
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs kea)
+        {
+            Console.WriteLine("key down");
+        }
+
+        private void MainWindow_KeyUp(object sender, KeyEventArgs kea)
+        {
+            Console.WriteLine("key up");
+        }
+    }
+}
