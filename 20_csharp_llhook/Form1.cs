4adcd19888fbd68e57379d95ebcb1356637cbb63
Sid Spry <github@sid.aeam.us>
2021-04-19 15:55:52 -0500

    LL hooks working.

--- 20_csharp_llhook/Form1.cs
+++ 20_csharp_llhook/Form1.cs
@@ -1,20 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using static PInvoke.Kernel32;
+using static PInvoke.User32;
 
 namespace _20_csharp_llhook
 {
+    [StructLayout(LayoutKind.Sequential)]
+    public class KBDLLHOOKSTRUCT
+    {
+        public uint vkCode;
+        public uint scanCode;
+        public KBDLLHOOKSTRUCTFlags flags;
+        public uint time;
+        public UIntPtr dwExtraInfo;
+    }
+
+    [Flags]
+    public enum KBDLLHOOKSTRUCTFlags : uint {
+        LLKHF_EXTENDED = 0x01,
+        LLKHF_INJECTED = 0x10,
+        LLKHF_ALTDOWN = 0x20,
+        LLKHF_UP = 0x80,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MSLLHOOKSTRUCT
+    {
+        public POINT pt;
+        public int mouseData;
+        public int flags;
+        public int time;
+        public UIntPtr dwExtraInfo;
+    }
+
     public partial class Form1 : Form
     {
+        SafeHookHandle mouse, keyboard;
+
         public Form1()
         {
             InitializeComponent();
+            AllocConsole();
+            mouse = SetWindowsHookEx(WindowsHookType.WH_KEYBOARD_LL, llKeyboard, 
+                GetModuleHandle(null), 0);
+            keyboard = SetWindowsHookEx(WindowsHookType.WH_MOUSE_LL, llMouse,
+                GetModuleHandle(null), 0);
+        }
+
+        int llKeyboard(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            var kbll = (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+            Console.WriteLine((WindowMessage)wParam + " " + kbll.flags + " " + kbll.dwExtraInfo);
+            return CallNextHookEx(keyboard.DangerousGetHandle(), nCode, wParam, lParam);
+        }
+
+        int llMouse(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            var msll = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+            Console.WriteLine((WindowMessage)wParam + " " + msll.pt.X + " " + msll.pt.Y);
+            return CallNextHookEx(keyboard.DangerousGetHandle(), nCode, wParam, lParam);
         }
 
         protected override void WndProc(ref Message m)
