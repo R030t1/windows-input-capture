5707b78ff79795d8f8aea429861e530c719c8d5f
Sid Spry <github@sid.aeam.us>
2021-04-19 16:45:53 -0500

    Start rawinput example

---
+++ 30_csharp_rawinput/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PInvoke.Kernel32;
+using static PInvoke.User32;
+
+namespace _30_csharp_rawinput
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AllocConsole();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+        }
+    }
+}
